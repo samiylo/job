5ab254498cef4ca08d6137549c725cd041a9d755
samiylo <skryshu@gmail.com>
2020-04-17 12:31:17 -0500

    Added While Loop

--- lib/job_posting/cli.rb
+++ lib/job_posting/cli.rb
@@ -6,12 +6,21 @@ class Cli
         puts "1 - List all jobs."
         puts "2 - Search job by area."
         puts "3 - Pick random job"
+        puts "Or type MENU for menu, or EXIT to exit the app."
 
     end
 
     def run
         puts "Hello! Welcome to my Kingwood job search app."
         menu 
+
+        user_input = gets.strip.downcase
+
+        while user_input != "exit"
+
+        end
+        
+
     end
 
 
