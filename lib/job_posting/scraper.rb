64a2acd98462e3fb0724768b3b8fc060507a4047
samiylo <skryshu@gmail.com>
2020-04-17 11:20:57 -0500

    Scraper Progress

--- lib/job_posting/scraper.rb
+++ lib/job_posting/scraper.rb
@@ -4,6 +4,13 @@ class JobObjects
         url = "https://www.indeed.com/l-Kingwood,-TX-jobs.html"
         raw_html = HTTParty.get(url)
         parsed_page = Nokogiri::HTML(raw_html)
+
+        job_cards = parsed_page.css('div.jobsearch-SerpJobCard')  #15 JOBS
+
+        job_cards.each do |job_listing|
+
+        end
+        
         binding.pry
 
     end
