35d5c2e2575f25afecf06f1059bdfc9729ade834
samiylo <skryshu@gmail.com>
2020-04-17 13:18:05 -0500

    Finished most menu

--- lib/job_posting/cli.rb
+++ lib/job_posting/cli.rb
@@ -5,7 +5,7 @@ class Cli
         puts "=================================================="
         puts "What would you like to do?"
         puts "1 - List all jobs."
-        puts "2 - Search job by area."
+        puts "2 - Pick a job posting."
         puts "3 - Pick random job"
         puts "Or type MENU for menu, or EXIT to exit the app."
         puts "=================================================="
@@ -13,7 +13,9 @@ class Cli
     end
 
     def random_job
+        puts "=================================================="
         JobObjects.random
+        
     end
 
     def run
@@ -29,12 +31,17 @@ class Cli
             user_input = gets.strip.downcase
             if user_input == "1"
                 JobObjects.all_jobs
+                menu
             elsif user_input == "2"
 
             elsif user_input == "3"
                 random_job
+                menu
+                
+            elsif user_input == "menu"
+                menu
             else
-                "Please pick an option from the menu."
+                puts "Please pick an option from the menu."
 
             end
 
